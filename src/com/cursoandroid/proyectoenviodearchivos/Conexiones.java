78d0e7431786163b7cf6fd45b487fced3dd506c6
Bruno Fernandez <bruno.sistemas@outlook.com>
2016-10-19 16:48:59 -0300

    Agregada la parte de poner una imagen en la pantalla.El boton siguiente crashea la app

---
+++ src/com/cursoandroid/proyectoenviodearchivos/Conexiones.java
@@ -0,0 +1,24 @@
+package com.cursoandroid.proyectoenviodearchivos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Conexiones extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_conexiones);
+//		Bundle extras = getIntent().getExtras();
+//		if(extras==null){
+//			//return;
+//		}
+//		String valor1 = extras.getString(Intent.EXTRA_TEXT);
+//		if (valor1 != null) {
+//		// …hacer algo con los datos
+//		}
+	}
+}

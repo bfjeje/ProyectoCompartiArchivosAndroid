870bf5f1c5c430b3a05b4015acdd5c8ede2f0396
Bruno Fernandez <bruno.sistemas@outlook.com>
2016-10-19 17:38:10 -0300

    Revert "Creado el layout de la pantalla principal"
    This reverts commit 980d5727a211ac6ae26068ac09c2a44adc11a9b3.

--- gen/com/cursoandroid/proyectoenviodearchivos/R.java
+++ gen/com/cursoandroid/proyectoenviodearchivos/R.java
@@ -22,35 +22,24 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_insert_drive_file_white_18dp=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int logo=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int bienvenida_text_view=0x7f090000;
-        public static final int boton_archivo=0x7f090003;
-        public static final int boton_camara=0x7f090001;
-        public static final int boton_imagen=0x7f090002;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f040000;
-        public static final int activity_pantalla_principal=0x7f040001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
-        public static final int pantalla_principal=0x7f080001;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
+        public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
-        /**  Pantalla Principal 
-         */
-        public static final int bienvenido=0x7f060003;
-        public static final int que_desea=0x7f060004;
-        public static final int title_activity_pantalla_principal=0x7f060002;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
